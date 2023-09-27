a8e6be3b4e5c3a78248c44b47acbca5a4c8715a4
zero <yuluvilla777@gmail.com>
2023-09-26 22:27:19 -0600

    correcion de titulo

--- Agrotics/Agrotics.Designer.cs
+++ Agrotics/Agrotics.Designer.cs
@@ -334,7 +334,6 @@ namespace Agrotics
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = resources.GetString("$this.Text");
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
